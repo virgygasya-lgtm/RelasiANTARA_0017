76a21fe76f715cef4efd74a4bfe4e00698e3b4bc
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:47:31 +0700

    manusia.h

--- manusia.h
+++ manusia.h
@@ -1,13 +1,21 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+
+using namespace std;
+
 class manusia {
 public:
     string name;
     jantung varJantung;
 
-        manusia(string pName)
+    manusia(string pName)
         : name(pName) {
         cout << name << " hidup\n";
     }
 
-        ~manusia() {
+    ~manusia() {
         cout << name << " mati\n";
     }
+};
+
+#endif

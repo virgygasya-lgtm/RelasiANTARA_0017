6bf0d2844781df32a1fd8d24fea9595461dc6f2a
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:33:11 +0700

    membuatfungsitambahpasien.cpp

--- membuatfungsitambahpasien.cpp
+++ membuatfungsitambahpasien.cpp
@@ -48,3 +48,8 @@ void pasien::cetakDokter() {
     }
     cout << endl;
 }
+
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}

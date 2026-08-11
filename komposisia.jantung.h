f6fd8b415e60b803fdbe35b56c5cf149934a37d9
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:44:15 +0700

    komposisia.jantung.h

--- komposisia.jantung.h
+++ komposisia.jantung.h
@@ -1,10 +1,17 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
 class jantung {
 public:
-
     jantung() {
         cout << "jantung dihidupkan\n";
     }
 
-        ~jantung() {
+    ~jantung() {
         cout << "jantung dimatikan\n";
     }
+};
+
+#endif

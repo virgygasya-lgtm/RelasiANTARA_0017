5b9a3cb334a84fb5fdde7723d41a7e5801bc5ce5
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:36:16 +0700

    mencetakdatadokterdanpasien.cpp

--- mencetakdatadokterdanpasien.cpp
+++ mencetakdatadokterdanpasien.cpp
@@ -71,3 +71,8 @@ int main() {
         varDokter1->tambahPasien(varPasien1);
     varDokter1->tambahPasien(varPasien2);
     varDokter2->tambahPasien(varPasien1); 
+
+        varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();

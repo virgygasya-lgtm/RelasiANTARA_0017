f7bc1ebe18235f378873a90c81c56300376517f9
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:16:09 +0700

    membuatdestructoribu.cpp

--- membuatdestructoribu.cpp
+++ membuatdestructoribu.cpp
@@ -10,3 +10,7 @@ public:
         ibu(string pNama) :nama(pNama) {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
+
+        ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }

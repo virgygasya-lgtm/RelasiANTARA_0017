cb805592c7200192ea46fbfacef192163751f416
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:36:56 +0700

    menghapusobjek.cpp

--- menghapusobjek.cpp
+++ menghapusobjek.cpp
@@ -76,3 +76,8 @@ int main() {
     varDokter2->cetakPasien();
     varPasien1->cetakDokter();
     varPasien2->cetakDokter();
+
+        delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;

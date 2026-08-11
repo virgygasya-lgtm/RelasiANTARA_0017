84f5beaa1128caa1ea441f8d48c4fe6bc576b328
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-11 10:14:49 +0700

    membuatconstructoribu.cpp

---
+++ membuatconstructoribu.cpp
@@ -0,0 +1,12 @@
+#ifndef IBU_H
+#define IBU_H
+#include <vector>
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+        ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
